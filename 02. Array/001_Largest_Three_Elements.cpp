49399c110d18dff906d568e9099cead111404a42
Talibiq123 <talibsaeed90@gmail.com>
2024-07-24 12:37:57 +0530

    Three Distinct Largest Elements

--- 02. Array/001_Largest_Three_Elements.cpp
+++ 02. Array/001_Largest_Three_Elements.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
+#include <climits>
 
 using namespace std;
 
 int main() {
-    int arr[] = {2, 4, 1, 7, 3, 0, 8, 3, 9, 6};
+    int arr[] = {6, 8, 1, 9, 2, 1, 10, 10};
+    int size = sizeof(arr) / sizeof(arr[0]); 
+
+    int largest = INT_MIN;
+    int second_largest = INT_MIN;
+    int third_largest = INT_MIN;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > largest) {
+            third_largest = second_largest;
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] > second_largest && arr[i] < largest) {
+                third_largest = second_largest;
+                second_largest = arr[i];
+        } else if (arr[i] > third_largest && arr[i] < second_largest) {
+            third_largest = arr[i];
+        }
+    }
+
+    if (third_largest == INT_MIN) {
+        cout << "There are no three distinct elements.";
+    } else {
+        cout << "Largest Element: " << largest << endl;
+        cout << "Second Largest Element: " << second_largest << endl;
+        cout << "Third Largest Element: " << third_largest << endl;
+    }
+
     return 0;
 }
